3317e47fd1c0a16e55b7cf88f5f912772456f7a3
Chip Freeney <cfreeney@lat-cfreeney1.cb.careerbuilder.com careerbuilder.com>
2015-03-04 20:11:50 -0500

    added URL showing up in controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -69,6 +69,6 @@ class WorksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def work_params
-      params.require(:work).permit(:title, :description, :image)
+      params.require(:work).permit(:title, :description, :image, :url)
     end
 end
